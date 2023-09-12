b4073d0eedb3dcd19f8f0ed9c7a813addb3833c8
Mann Jadwani <mannjadwani2004@gmail.com>
2023-09-12 22:01:35 +0530

    added expressjs

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,6 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
@@ -17,3 +13,30 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const express = require("express");
+const cors = require('cors')
+
+const stripe = require("stripe")('sk_test_51NpW2kSH364fmD4D3bQ5jdBoa8XJwniZpYyV3sFCkIV19TU18EwJhGs2FZhB1NcMxD2MvCohCmvnsyLiJ2PYlaH500hPNoWOAg')
+
+// api 
+const app = express();
+//app config 
+
+// middlewares
+app.use(cors({origin:true}))
+app.use(express.json())
+
+//api routes
+
+app.get('/',(request,response)=>{
+    response.status(200).send(('hello world'))
+})
+
+// listen command 
+
+exports.api = onRequest(app)
