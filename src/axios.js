50674e74a9ac070d58e2a90a7c552f585533bbca
Mann Jadwani <mannjadwani2004@gmail.com>
2023-10-09 20:11:21 +0530

    fixed firebase error

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance=  axios.create({
-    baseURL: '..' //api
+    baseURL: 'http://127.0.0.1:5001/clone-e260c/us-central1/app' //api
 })
 
 export default instance
